da0add868c2c1c6ab6c2abaa7d3b91c2e3c9150f
Roman Volozhanin <rsvolozhanin@gmail.com>
2022-10-03 23:11:45 +0300

    add login front

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -10,6 +10,7 @@ env.read_env()
 class Settings(BaseSettings):
     SECRET_KEY: str = secrets.token_urlsafe(32)
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 8
+    ALLOWED_ORIGINS: list = ["http://127.0.0.1:3000", "http://localhost:3000", "https://www.beatport.com/"]
     ALGORITHM: str = "HS256"
     DB_URL: str = "sqlite:///db_test.db"
 
--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,24 @@
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.api import login, users
+from app.core.config import settings
 
 
 def create_application() -> FastAPI:
     application = FastAPI()
     application.include_router(users.router, prefix="/users", tags=["users"])
     application.include_router(login.router, tags=["login"])
+    application.add_middleware(
+        CORSMiddleware,
+        # allow_origins=["http://127.0.0.1:3000", "http://localhost:3000"],
+        # allow_origins=settings.ALLOWED_ORIGINS,
+        allow_origins=["*"],
+        allow_credentials=True,
+        allow_methods=["GET", "POST", "PUT", "PATCH", "OPTIONS", "DELETE"],
+        allow_headers=["*"],
+    )
     return application
 
 
