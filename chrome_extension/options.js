921d5498202dc87c2e0cac3adc51acdb397676e1
Roman Volozhanin <rsvolozhanin@gmail.com>
2022-10-05 09:45:50 +0300

    linter's work

--- chrome_extension/options.js
+++ chrome_extension/options.js
@@ -5,49 +5,50 @@ let startSession = document.getElementById("loginMe");
 showToken();
 
 function showToken() {
-	chrome.storage.sync.get('access_token', function(result) {
-		let token =  result.access_token;
-		document.getElementById("statusLogin").innerText = "Your Token:\n" + token;
-	});
+  chrome.storage.sync.get('access_token', function (result) {
+    let token = result.access_token;
+    document.getElementById("statusLogin").innerText = "Your Token:\n" + token;
+  });
 }
 
 async function LoginMe(username, password) {
-	let details = {
-		'username': username,
-		'password': password
-	};
+  let details = {
+    'username': username,
+    'password': password
+  };
 
-	let formBody = [];
-	for (const property in details) {
-		const encodedKey = encodeURIComponent(property);
-		const encodedValue = encodeURIComponent(details[property]);
-		formBody.push(encodedKey + "=" + encodedValue);
-	}
-	formBody = formBody.join("&");
+  let formBody = [];
+  for (const property in details) {
+    const encodedKey = encodeURIComponent(property);
+    const encodedValue = encodeURIComponent(details[property]);
+    formBody.push(encodedKey + "=" + encodedValue);
+  }
+  formBody = formBody.join("&");
 
-	const response = await fetch(`${api_url}/login/access-token`, {
-		method: 'POST',
-		headers: {
-			'Accept': 'application/json',
-			'Content-Type': 'application/x-www-form-urlencoded'
-		},
-		body: formBody
-	}).then(response => response.json()).then(data => {
-		const token = data["access_token"]
-		if (token) {
-			chrome.storage.sync.set({ access_token: data["access_token"] });
-		} else {
-			console.log(data)
-		}
+  const response = await fetch(`${api_url}/login/access-token`, {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    body: formBody
 
-	});
-	showToken();
+  }).then(response => response.json()).then(data => {
+    const token = data["access_token"]
+    if (token) {
+      chrome.storage.sync.set({access_token: data["access_token"]});
+    } else {
+      console.log(data.detail)
+    }
+
+  });
+  showToken();
 }
 
 startSession.addEventListener("click", async (event) => {
   event.preventDefault();
   let username = document.getElementById("usernameInput").value
   let password = document.getElementById("passwordInput").value
-  await LoginMe(username, password)
+  await LoginMe(username, password);
 
 });
--- chrome_extension/popup.js
+++ chrome_extension/popup.js
@@ -1,76 +1,81 @@
 let startSession = document.getElementById("start_session");
 
 startSession.addEventListener("click", async () => {
-  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+  let [tab] = await chrome.tabs.query({active: true, currentWindow: true});
   chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    func: testLogin,
+    target: {tabId: tab.id},
+    func: readAllReleases,
   });
 });
 
 function readAllReleases() {
-	function handleRelease(release) {
-		let new_release = {}
-		new_release["id"] = release.getAttribute("data-ec-id")
-		new_release["title"] = release.getAttribute("data-ec-name")
-		new_release["r_link"] = release.getElementsByClassName("horz-release-artwork-parent")[0].getElementsByTagName("a")[0].getAttribute("href")
-		new_release["session_position"] = release.getAttribute("data-ec-position")
+  chrome.storage.sync.get('access_token', function (result) {
+    let token = result.access_token;
+    console.log(token)
+  });
 
-		let raw_artists = release.getElementsByClassName("buk-horz-release-artists")[0].getElementsByTagName("a")
-		let artists = {}
-		for (const art of raw_artists) {
-			let art_id = art.getAttribute("data-artist")
-			artists[art_id] = {
-				art_link: art.getAttribute("href"),
-				art_title: art.innerText
-			}
-		}
-		new_release["artists"] = artists
-		
-		let raw_labels = release.getElementsByClassName("buk-horz-release-labels")[0].getElementsByTagName("a")
-		let labels = {}
-		for (const label of raw_labels) {
-			let label_id = label.getAttribute("data-label")
-			labels[label_id] = {
-				label_link: label.getAttribute("href"),
-				label_title: label.innerText
-			}
-		}
-		new_release["labels"] = labels
-		
-		new_release["date"] = release.getElementsByClassName("buk-horz-release-released")[0].innerText
-		
-		return new_release
-	}
-	
-	const cur_url = new URL(window.location.href)
-	if (cur_url.hostname !== "www.beatport.com") {
-		return
-	}
-	if (!(cur_url.pathname > "/genre/")) {
-		return
-	}
-	
-	const style_name = cur_url.pathname.split("/")[2]
-	const page = cur_url.searchParams.get('page')
-	const start_date = cur_url.searchParams.get('start-date')
-	const end_date = cur_url.searchParams.get('end-date')
-	
-	let new_releases = document.getElementsByClassName("bucket-item");
-	let releases = {}
-	for (const work_element of new_releases) {
-		let release = handleRelease(work_element)
-		if (release){
-			releases[release.id] = release
-		}
-	}
-	
-	let session_page = {
-		style_name: style_name,
-		page: page,
-		start_date: start_date,
-		end_date: end_date,
-		releases: releases
-	}
-	console.log(session_page)
+  function handleRelease(release) {
+    let new_release = {}
+    new_release["id"] = release.getAttribute("data-ec-id")
+    new_release["title"] = release.getAttribute("data-ec-name")
+    new_release["r_link"] = release.getElementsByClassName("horz-release-artwork-parent")[0].getElementsByTagName("a")[0].getAttribute("href")
+    new_release["session_position"] = release.getAttribute("data-ec-position")
+
+    let raw_artists = release.getElementsByClassName("buk-horz-release-artists")[0].getElementsByTagName("a")
+    let artists = {}
+    for (const art of raw_artists) {
+      let art_id = art.getAttribute("data-artist")
+      artists[art_id] = {
+        art_link: art.getAttribute("href"),
+        art_title: art.innerText
+      }
+    }
+    new_release["artists"] = artists
+
+    let raw_labels = release.getElementsByClassName("buk-horz-release-labels")[0].getElementsByTagName("a")
+    let labels = {}
+    for (const label of raw_labels) {
+      let label_id = label.getAttribute("data-label")
+      labels[label_id] = {
+        label_link: label.getAttribute("href"),
+        label_title: label.innerText
+      }
+    }
+    new_release["labels"] = labels
+
+    new_release["date"] = release.getElementsByClassName("buk-horz-release-released")[0].innerText
+
+    return new_release
+  }
+
+  const cur_url = new URL(window.location.href)
+  if (cur_url.hostname !== "www.beatport.com") {
+    return
+  }
+  if (!(cur_url.pathname > "/genre/")) {
+    return
+  }
+
+  const style_name = cur_url.pathname.split("/")[2]
+  const page = cur_url.searchParams.get('page')
+  const start_date = cur_url.searchParams.get('start-date')
+  const end_date = cur_url.searchParams.get('end-date')
+
+  let new_releases = document.getElementsByClassName("bucket-item");
+  let releases = {}
+  for (const work_element of new_releases) {
+    let release = handleRelease(work_element)
+    if (release) {
+      releases[release.id] = release
+    }
+  }
+
+  let session_page = {
+    style_name: style_name,
+    page: page,
+    start_date: start_date,
+    end_date: end_date,
+    releases: releases
+  }
+  console.log(session_page)
 }
