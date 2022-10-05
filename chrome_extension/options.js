3d3f6d546fd97f7249685910690589cabd81a6f4
Roman Volozhanin <rsvolozhanin@gmail.com>
2022-10-05 08:55:00 +0300

    show token

--- chrome_extension/options.js
+++ chrome_extension/options.js
@@ -2,15 +2,20 @@ const api_url = "http://127.0.0.1:8007"
 
 let startSession = document.getElementById("loginMe");
 
+showToken();
+
+function showToken() {
+	chrome.storage.sync.get('access_token', function(result) {
+		let token =  result.access_token;
+		document.getElementById("statusLogin").innerText = "Your Token:\n" + token;
+	});
+}
+
 async function LoginMe(username, password) {
 	let details = {
 		'username': username,
 		'password': password
 	};
-
-	chrome.storage.sync.get('access_token', function(result) {
-		console.log('Value currently is ' + result.access_token);
-	});
 
 	let formBody = [];
 	for (const property in details) {
@@ -20,29 +25,29 @@ async function LoginMe(username, password) {
 	}
 	formBody = formBody.join("&");
 
-		const response = await fetch(`${api_url}/login/access-token`, {
-			method: 'POST',
-			headers: {
-				'Accept': 'application/json',
-				'Content-Type': 'application/x-www-form-urlencoded'
-			},
-			body: formBody
-		}).then(response => response.json()).then(data => {
-			const token = data["access_token"]
-			if (token) {
-				chrome.storage.sync.set({ access_token: data["access_token"] });
-			} else {
-				console.log(data)
-			}
+	const response = await fetch(`${api_url}/login/access-token`, {
+		method: 'POST',
+		headers: {
+			'Accept': 'application/json',
+			'Content-Type': 'application/x-www-form-urlencoded'
+		},
+		body: formBody
+	}).then(response => response.json()).then(data => {
+		const token = data["access_token"]
+		if (token) {
+			chrome.storage.sync.set({ access_token: data["access_token"] });
+		} else {
+			console.log(data)
+		}
 
-		});
-
+	});
+	showToken();
 }
 
 startSession.addEventListener("click", async (event) => {
   event.preventDefault();
   let username = document.getElementById("usernameInput").value
   let password = document.getElementById("passwordInput").value
-  let data = await LoginMe(username, password)
+  await LoginMe(username, password)
 
 });
