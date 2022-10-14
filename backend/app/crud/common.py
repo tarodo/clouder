85bfa66a0a53a05c332afa8894e8bf828b5e00fe
Roman Volozhanin <rsvolozhanin@gmail.com>
2022-10-14 23:13:10 +0300

    moved functions of CRUD to common

---
+++ backend/app/crud/common.py
@@ -0,0 +1,40 @@
+from fastapi.encoders import jsonable_encoder
+from sqlmodel import Session, select
+from sqlmodel import SQLModel
+
+
+def read_by_id(db: Session, Model, elem_id: int) -> SQLModel | None:
+    """Read one element by id"""
+    elem = select(Model).where(Model.id == elem_id)
+    elem = db.exec(elem).one_or_none()
+    return elem
+
+
+def create(db: Session, Model, payload: SQLModel) -> SQLModel:
+    """Create an element"""
+    element = Model(**payload.dict())
+    db.add(element)
+    db.commit()
+    db.refresh(element)
+    return element
+
+
+def update(db: Session, db_obj: SQLModel, payload: SQLModel) -> SQLModel:
+    """Update style's data"""
+    obj_data = jsonable_encoder(db_obj)
+    update_data = payload.dict(exclude_unset=True, exclude_none=True)
+    for field in obj_data:
+        if field in update_data:
+            new_data = update_data[field]
+            setattr(db_obj, field, new_data)
+    db.add(db_obj)
+    db.commit()
+    db.refresh(db_obj)
+    return db_obj
+
+
+def remove(db: Session, db_obj: SQLModel) -> SQLModel:
+    """Remove style from DB"""
+    db.delete(db_obj)
+    db.commit()
+    return db_obj
--- backend/app/crud/styles.py
+++ backend/app/crud/styles.py
@@ -1,6 +1,6 @@
 from app.models import Style, StyleIn, StyleUpdate
-from fastapi.encoders import jsonable_encoder
-from sqlmodel import Session, select
+from sqlmodel import Session, select, SQLModel
+from app.crud import common
 
 
 def read_by_name(db: Session, name: str) -> Style | None:
@@ -10,38 +10,18 @@ def read_by_name(db: Session, name: str) -> Style | None:
     return style
 
 
-def read_by_id(db: Session, style_id: int) -> Style | None:
-    """Read one style by id"""
-    style = select(Style).where(Style.id == style_id)
-    style = db.exec(style).one_or_none()
-    return style
+def read_by_id(db: Session, style_id: int) -> SQLModel | None:
+    return common.read_by_id(db, Style, style_id)
 
 
-def create(db: Session, payload: StyleIn) -> Style:
-    """Create a style"""
-    style = Style(**payload.dict())
-    db.add(style)
-    db.commit()
-    db.refresh(style)
-    return style
+def create(db: Session, payload: StyleIn) -> SQLModel:
+    return common.create(db, Style, payload)
 
 
-def update(db: Session, db_obj: Style, payload: StyleUpdate) -> Style:
-    """Update style's data"""
-    obj_data = jsonable_encoder(db_obj)
-    update_data = payload.dict(exclude_unset=True, exclude_none=True)
-    for field in obj_data:
-        if field in update_data:
-            new_data = update_data[field]
-            setattr(db_obj, field, new_data)
-    db.add(db_obj)
-    db.commit()
-    db.refresh(db_obj)
-    return db_obj
+def update(db: Session, db_obj: Style, payload: StyleUpdate) -> SQLModel:
+    return common.update(db, db_obj, payload)
 
 
-def remove(db: Session, db_obj: Style) -> Style:
-    """Remove style from DB"""
-    db.delete(db_obj)
-    db.commit()
-    return db_obj
+def remove(db: Session, db_obj: Style) -> SQLModel:
+    return common.remove(db, db_obj)
+
