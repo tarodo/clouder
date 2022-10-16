f4b1748de4c5bc3927b8641ec46de4e5e3905fa2
Roman Volozhanin <rsvolozhanin@gmail.com>
2022-10-16 13:30:53 +0300

    correct error message model

--- backend/app/api/tools.py
+++ backend/app/api/tools.py
@@ -5,5 +5,5 @@ from fastapi import HTTPException
 
 
 def raise_400(err: Enum) -> None:
-    message = Message(err=str(err), message=str(err.value)).dict()
+    message = Message(type=str(err), msg=str(err.value)).dict()
     raise HTTPException(status_code=400, detail=message)
--- backend/app/models/tools.py
+++ backend/app/models/tools.py
@@ -2,8 +2,8 @@ from sqlmodel import Field, SQLModel
 
 
 class Message(SQLModel):
-    err: str = Field(..., description="Error code")
-    message: str = Field(..., description="Full error message")
+    type: str = Field(..., description="Error code")
+    msg: str = Field(..., description="Full error message")
 
 
 class ErrorMessage(SQLModel):
