d93b03c8afc4ac8c77dcdd6c03f4b8f4106b096d
Roman Volozhanin <rsvolozhanin@gmail.com>
2022-10-23 19:06:35 +0300

    add id to pack model

--- backend/app/models/pack.py
+++ backend/app/models/pack.py
@@ -1,11 +1,13 @@
+from sqlalchemy import UniqueConstraint
+
 from app.models.periods import Period
 from app.models.styles import Style
 from sqlmodel import Field, Relationship, SQLModel
 
 
 class PackBase(SQLModel):
-    style_id: int = Field(foreign_key="style.id", primary_key=True)
-    period_id: int = Field(foreign_key="period.id", primary_key=True)
+    style_id: int = Field(foreign_key="style.id")
+    period_id: int = Field(foreign_key="period.id")
     sheets_count: int = Field(..., ge=1)
 
 
@@ -14,6 +16,9 @@ class PackBaseDB(PackBase):
 
 
 class Pack(PackBaseDB, table=True):
+    __table_args__ = (UniqueConstraint("style_id", "period_id"),)
+    id: int = Field(primary_key=True)
+
     style: Style = Relationship(back_populates="packs")
     period: Period = Relationship(back_populates="packs")
 
@@ -23,7 +28,7 @@ class PackInDB(PackBaseDB):
 
 
 class PackOut(PackBaseDB):
-    pass
+    id: int = Field(...)
 
 
 class PackUpdate(PackBase):
