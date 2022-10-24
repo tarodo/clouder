5af3a54b6882243b276a73e3f1d963df857a5f00
Roman Volozhanin <rsvolozhanin@gmail.com>
2022-10-24 20:34:31 +0300

    add pack api read by id tests

--- backend/tests/api/test_packs.py
+++ backend/tests/api/test_packs.py
@@ -170,3 +170,42 @@ def test_pack_read_empty(client: TestClient, db: Session, random_user: User) -> 
     assert r.status_code == 400
     one_pack = r.json()
     assert one_pack["detail"]["type"] == str(PacksErrors.NotEnoughParams)
+
+
+def test_pack_read_id(client: TestClient, db: Session, random_user: User) -> None:
+    user_pack = create_random_pack(db, random_user)
+    pack_id = user_pack.id
+    user_token_headers = get_authentication_token_from_email(
+        client=client, email=random_user.email, db=db
+    )
+    r = client.get(f"/packs/{pack_id}", headers=user_token_headers)
+    assert 200 <= r.status_code < 300
+    one_pack = r.json()
+    assert one_pack
+    assert one_pack["id"] == pack_id
+
+
+def test_pack_read_id_by_admin(
+    client: TestClient, db: Session, superuser_token_headers
+) -> None:
+    random_user = create_random_user(db)
+    user_pack = create_random_pack(db, random_user)
+    pack_id = user_pack.id
+    r = client.get(f"/packs/{pack_id}", headers=superuser_token_headers)
+    assert 200 <= r.status_code < 300
+    one_pack = r.json()
+    assert one_pack
+    assert one_pack["id"] == pack_id
+
+
+def test_pack_read_id_wrong(client: TestClient, db: Session, random_user: User) -> None:
+    another_user = create_random_user(db)
+    user_pack = create_random_pack(db, another_user)
+    pack_id = user_pack.id
+    user_token_headers = get_authentication_token_from_email(
+        client=client, email=random_user.email, db=db
+    )
+    r = client.get(f"/packs/{pack_id}", headers=user_token_headers)
+    assert r.status_code == 400
+    one_pack = r.json()
+    assert one_pack["detail"]["type"] == str(PacksErrors.UserHasNoAccess)
