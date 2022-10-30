7d284f0477587fad91f74a8b1031021e8cd36cd6
Roman Volozhanin <rsvolozhanin@gmail.com>
2022-10-30 08:26:20 +0300

    CLOUD-10 add beatport artist model

---
+++ backend/app/models/beatport/artists.py
@@ -0,0 +1,31 @@
+from pydantic import constr
+from sqlmodel import Field, SQLModel
+
+name_con = constr(min_length=1, max_length=127)
+url_con = constr(min_length=1)
+
+
+class ArtistBase(SQLModel):
+    name: name_con = Field(index=True, nullable=False)
+    url: url_con = Field(nullable=False)
+    bp_id: int = Field(index=True, nullable=False, sa_column_kwargs={"unique": True})
+
+
+class ArtistBaseDB(ArtistBase):
+    pass
+
+
+class Artist(ArtistBaseDB, table=True):
+    id: int = Field(primary_key=True)
+
+
+class ArtistInDB(ArtistBaseDB):
+    pass
+
+
+class ArtistOut(ArtistBaseDB):
+    id: int = Field(...)
+
+
+class ArtistInApi(ArtistBase):
+    pass
