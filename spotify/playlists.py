b06674bed5ac3bd58996322203e2ea3644698611
Roman Volozhanin <rsvolozhanin@gmail.com>
2023-08-06 21:30:36 +0400

    connect tracks bp -> spotify

--- spotify/playlists.py
+++ spotify/playlists.py
@@ -21,14 +21,8 @@ def create_playlist(sp: Spotify, title: str) -> (str, str):
     return playlist["id"], playlist["external_urls"]["spotify"]
 
 
-def create_playlist_from_bp(payload: PlaylistIn):
-    sp = create_sp()
-    playlist_id, playlist_url = create_playlist(sp, payload.name)
-    return playlist_url
-
-
-def get_track_by_isrc(isrc: str) -> SPTrack | None:
-    sp = create_sp()
+def get_track_by_isrc(isrc: str, sp: Spotify = None) -> SPTrack | None:
+    sp = sp if sp else create_sp()
     track_result = sp.search(q=f"isrc:{isrc}", type="track", limit=1)
     tracks = track_result["tracks"]["items"]
     if tracks:
@@ -38,6 +32,30 @@ def get_track_by_isrc(isrc: str) -> SPTrack | None:
         url = sp_track["external_urls"]["spotify"]
         return SPTrack(name=title, sp_id=track_id, url=url)
     return None
+
+
+def create_playlist_from_bp(payload: PlaylistIn):
+    sp = create_sp()
+    playlist_id, playlist_url = create_playlist(sp, payload.name)
+    tracks_ids = []
+    not_found = []
+    for track in payload.tracks:
+        sp_track = get_track_by_isrc(track.isrc, sp)
+        if sp_track:
+            tracks_ids.append(sp_track.sp_id)
+        else:
+            not_found.append(track)
+    print(tracks_ids)
+    print(f"{not_found=}")
+    pack_size = 100
+    parts = [
+        tracks_ids[i * pack_size : (i + 1) * pack_size]
+        for i in range(len(tracks_ids) // pack_size + 1)
+    ]
+    for part in parts:
+        print(f"{part=}")
+        sp.playlist_add_items(playlist_id, part)
+    return playlist_url
 
 
 if __name__ == "__main__":
