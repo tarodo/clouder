5078821ab19bdb59e1fb83198e4d1d214eaff13e
tarodo <rsvolozhanin@gmail.com>
2023-12-17 21:34:21 +0400

    feat: collect tracks for a week

--- beatport/week_tracks.py
+++ beatport/week_tracks.py
@@ -2,9 +2,13 @@ import json
 import os
 from pathlib import Path
 from pprint import pprint
+from environs import Env
+import requests
 
 from week_collector import DATA_DIR, BP_STYLES
 
+
+TRACK_URL = "https://api.beatport.com/v4/catalog/releases/"
 
 def collect_essential_week(raw_dir_path: Path):
     releases = []
@@ -52,32 +56,49 @@ def collect_essential_week(raw_dir_path: Path):
     return releases
 
 
-def collect_whole_week(week_dir_path: Path):
-    raw_dir_path = week_dir_path / "week_raw"
-    result_path = week_dir_path / "full_week.json"
+def handle_one_release(release: dict, raw_tracks_path: Path, bp_token: str):
+    url = f"{TRACK_URL}{release.get('release_id')}/tracks"
+    params = {
+        "page": 1,
+        "per_page": 100,
+    }
+    headers = {"Authorization": f"Bearer {bp_token}"}
+    r = requests.get(url, params=params, headers=headers)
+    r.raise_for_status()
+    tracks = r.json()
+    pprint(tracks)
+    with open(f"{raw_tracks_path}/{release.get('release_id')}.json", 'w') as f:
+        json.dump(tracks, f, indent=4)
 
-    releases = collect_essential_week(raw_dir_path)
-    with open(result_path, 'w') as f:
-        json.dump(releases, f, indent=4)
+
+def handle_week_tracks(full_week_path: Path, raw_tracks_path: Path, bp_token: str):
+    with open(full_week_path, 'r') as f:
+        releases = json.load(f)
+
+    for release in releases:
+        handle_one_release(release, raw_tracks_path, bp_token)
 
 
-def handle_week(week_num: int, style_id: int):
+def handle_week(week_num: int, style_id: int, bp_token: str):
     week_num = str(week_num).zfill(2)
     style_name = BP_STYLES.get(style_id)
+
     releases_path = Path(DATA_DIR) / style_name / week_num
-    collect_whole_week(releases_path)
+    raw_dir_path = releases_path / "week_raw"
+    full_week_path = releases_path / "full_week.json"
 
+    raw_tracks_path = releases_path / "tracks_raw"
+    os.makedirs(raw_tracks_path, exist_ok=True)
 
-def handle_week_tracks(week_num: int):
-    folder_path = f"{DATA_DIR}/{week_num}"
+    releases = collect_essential_week(raw_dir_path)
+    with open(full_week_path, 'w') as f:
+        json.dump(releases, f, indent=4)
 
-    with open(f"{folder_path}/full_week.json", 'r') as f:
-        releases = json.load(f)
-    print(len(releases))
-    tracks = []
-    for release in releases:
-        pass
+    handle_week_tracks(full_week_path, raw_tracks_path, bp_token)
 
 
 if __name__ == "__main__":
-    handle_week(9, 1)
+    env = Env()
+    env.read_env()
+    bp_token = env.str("BP_TOKEN")
+    handle_week(9, 1, bp_token)
